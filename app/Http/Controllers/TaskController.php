bed7d68a4979910362a2325a7b6390a0476a05ae
keigo5050 <keigo111kokoa@gmail.com>
2020-11-19 09:52:46 +0900

    タスク登録API実装

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -11,9 +11,14 @@ class TaskController extends Controller
     {
         return Task::all();
     }
-    
+
     public function show(Task $task)
     {
         return $task;
+    }
+
+    public function store(Request $request)
+    {
+        return Task::create($request->all());
     }
 }
